8b027732c366d5735d0dd4ca44378b24b233c1cb
martamihajlovic <mihajlovcmarta@gmail.com>
2022-09-12 14:16:19 +0200

    bez routera

--- mihajlovicsoft/src/main.js
+++ mihajlovicsoft/src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+// import VueRouter from 'vue-router'
+// import Routes from './router/index.js'
 
-createApp(App).mount('#app')
+
+// Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+// const router = new VueRouter({
+//   routes: Routes
+// })
+
+new Vue({
+  el:'#app',
+  render: h => h(App),
+  // router: router
+}).$mount('#app')
---
+++ mihajlovicsoft/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>test testic</div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    components: {
+
+    }
+}
+</script>
